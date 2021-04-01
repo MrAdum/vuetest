6a2e41e79224cde82e8f3065848d3e7e14c7bef8
MrAdum <1014283424@qq.com>
2021-04-01 23:25:41 +0800

    init

---
+++ precache-manifest.e59eefb209aa6c1b37b46d1b688b0871.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4557a9ab90cd7cd93fa7",
+    "url": "/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "15335853b96294cfa58f8d8507e824e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8610e7df79d80c0c835",
+    "url": "/js/about.90b284f2.js"
+  },
+  {
+    "revision": "4557a9ab90cd7cd93fa7",
+    "url": "/js/app.f9b1c729.js"
+  },
+  {
+    "revision": "70c694d528e30e61f630",
+    "url": "/js/chunk-vendors.0e478c33.js"
+  },
+  {
+    "revision": "9589a2e769485a9039be09a91e817ced",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
